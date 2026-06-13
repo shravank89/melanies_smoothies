a66befd8d261785d262a6152c0a7e69cb11fa157
shravank89 <102520481+shravank89@users.noreply.github.com>
2026-06-13 23:39:29 +0530

    Fix typo in requests import for API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ if time_to_insert:
 
 
 import requests
-smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())

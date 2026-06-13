8428cd7e5cacbdca6280b7e03e4898fdd68b3a4c
shravank89 <102520481+shravank89@users.noreply.github.com>
2026-06-13 23:11:23 +0530

    Replace session retrieval method in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -14,7 +14,8 @@ title = st.text_input('Enter Your Name')
 st.write('The name on your smoothie will be:',title)
 name_on_order = title
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
